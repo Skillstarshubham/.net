70cf74a8b3a3965f407de8347ee0d3b8349ccfb5
skumar44 <shubham.it.glbajaj@gmail.com>
2025-01-07 15:49:32 +0530

    Commit

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Net;
 using Addresses;
 using ConsoleApp5;
 
@@ -12,45 +11,70 @@ namespace SqlConnectionApp
     {
         static void Main(string[] args)
         {
-            string connectionString = "data source=LIN-PF1BULQT\\SQLEXPRESS;initial catalog=AdventureWorksLT4;persist security info=True; Integrated Security=SSPI;";
+            string connectionString = "data source=Amrit;initial catalog=AdventureWorksLT;persist security info=True; Integrated Security=SSPI;";
 
-            try
+            // Retrieve addresses
+            IEnumerable<Address> addresses = GetAddresses(connectionString);
+            // Retrieve products
+            IEnumerable<Product> products = GetProducts(connectionString);
+
+            var fil = products.Where(p => p.ListPrice > 1000 & p.ListPrice < 1500).OrderByDescending(s => s.ListPrice).ThenBy(d => d.Name);
+
+            var qsx = products.GroupBy(p => p.Color).Distinct();
+
+
+
+            var sel1 = products.Where(s => s.SellEndDate == null).Select(p => new
             {
-                // Retrieve addresses
-                IEnumerable<Address> addresses = GetAddresses(connectionString);
 
-                // LINQ Query: Filter addresses by City
-                var filteredAddresses = addresses
+                Name = p.Name,
+                p.ProductID
+            });
+
+            foreach (var c in sel1)
+            {
+
+                //Console.WriteLine(c.ProductID);
+            }
+
+
+            var csting = products.Select(s => {
+                if (int.TryParse(s.Size, out int result))
+                {
+                    return (int?)result;
+
+                }
+                return null;
+
+            });
+
+            foreach (var p in csting)
+            {
+
+                // Console.WriteLine(p.Value.ToString());
+
+            }
+
+
+            // LINQ Query: Filter addresses by City
+            var filteredAddresses = addresses
                     .Where(a => a.CountryRegion == "Canada")
                     .OrderBy(a => a.AddressLine1)
                     .Select(a => new
                     {
                         a.AddressID,
-                        a.AddressLine1,
+                        col1 = a.AddressLine1 + a.AddressLine2 + a.CountryRegion + a.StateProvince,
                         a.City
                     });
 
-                // Display the filtered addresses
-                foreach (var address in filteredAddresses)
-                {
-                    //Console.WriteLine($"ID: {address.AddressID}, AddressLine1: {address.AddressLine1}, City: {address.City}");
-                }
-                IEnumerable<Product> products = GetProducts(connectionString);
-                var sel1 = products.Select(p => new
-                {
-                    Name=p.Name,
-                    Nsize=int.TryParse(p.Size,out int size) ? size : 0
-                });
-                foreach (var C in sel1)
-                {
-                    Console.WriteLine(C.Name,C.Nsize);
-                }
-   
-            }
-            catch (Exception ex)
+
+
+            //// Display the filtered addresses
+            foreach (var address in filteredAddresses)
             {
-                Console.WriteLine($"An error occurred: {ex.Message}");
+                //  Console.WriteLine($"ID: {address.AddressID}, AddressLine1: {address.col1}, City: {address.City}");
             }
+
         }
 
         static IEnumerable<Address> GetAddresses(string connectionString)
@@ -62,6 +86,8 @@ namespace SqlConnectionApp
                 connection.Open();
 
                 string address = "SELECT * FROM SalesLT.Address";
+                string product = "Select * from SalesLT.Product";
+
                 using (SqlCommand command = new SqlCommand(address, connection))
                 {
                     using (SqlDataReader reader = command.ExecuteReader())
@@ -88,7 +114,7 @@ namespace SqlConnectionApp
             return addresses;
         }
 
-    static IEnumerable<Product> GetProducts(string connectionString)
+        static IEnumerable<Product> GetProducts(string connectionString)
         {
             var products = new List<Product>();
 
@@ -130,5 +156,142 @@ namespace SqlConnectionApp
 
             return products;
         }
+
+        static IEnumerable<SalesOrderDetail> GetSalesOrderDetails(string connectionString)
+        {
+            var salesOrderDetails = new List<SalesOrderDetail>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT SalesOrderID, SalesOrderDetailID, OrderQty, ProductID, UnitPrice, UnitPriceDiscount, LineTotal, Rowguid, ModifiedDate FROM SalesLT.SalesOrderDetail";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            salesOrderDetails.Add(new SalesOrderDetail
+                            {
+                                SalesOrderID = reader.GetInt32(0),
+                                SalesOrderDetailID = reader.GetInt32(1),
+                                OrderQty = reader.GetInt16(2),
+                                ProductID = reader.GetInt32(3),
+                                UnitPrice = reader.GetDecimal(4),
+                                UnitPriceDiscount = reader.GetDecimal(5),
+                                LineTotal = reader.GetDecimal(6), // Computed column
+                                Rowguid = reader.GetGuid(7),
+                                ModifiedDate = reader.GetDateTime(8)
+                            });
+                        }
+                    }
+                }
+            }
+
+            return salesOrderDetails;
+        }
+
+
+        static IEnumerable<Customer> GetCustomers(string connectionString)
+        {
+            var customers = new List<Customer>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT CustomerID, NameStyle, Title, FirstName, MiddleName, LastName, Suffix, CompanyName, SalesPerson, EmailAddress, Phone, PasswordHash, PasswordSalt, Rowguid, ModifiedDate FROM SalesLT.Customer";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            customers.Add(new Customer
+                            {
+                                CustomerID = reader.GetInt32(0),
+                                NameStyle = reader.GetBoolean(1),
+                                Title = reader.IsDBNull(2) ? null : reader.GetString(2),
+                                FirstName = reader.GetString(3),
+                                MiddleName = reader.IsDBNull(4) ? null : reader.GetString(4),
+                                LastName = reader.GetString(5),
+                                Suffix = reader.IsDBNull(6) ? null : reader.GetString(6),
+                                CompanyName = reader.IsDBNull(7) ? null : reader.GetString(7),
+                                SalesPerson = reader.IsDBNull(8) ? null : reader.GetString(8),
+                                EmailAddress = reader.IsDBNull(9) ? null : reader.GetString(9),
+                                Phone = reader.IsDBNull(10) ? null : reader.GetString(10),
+                                PasswordHash = reader.GetString(11),
+                                PasswordSalt = reader.GetString(12),
+                                Rowguid = reader.GetGuid(13),
+                                ModifiedDate = reader.GetDateTime(14)
+                            });
+                        }
+                    }
+                }
+            }
+
+            return customers;
+        }
+
+        static IEnumerable<SalesOrderHeader> GetSalesOrderHeaders(string connectionString)
+        {
+            var salesOrderHeaders = new List<SalesOrderHeader>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = @"
+                SELECT 
+                    SalesOrderID, RevisionNumber, OrderDate, DueDate, ShipDate, Status, 
+                    OnlineOrderFlag, SalesOrderNumber, PurchaseOrderNumber, AccountNumber, 
+                    CustomerID, ShipToAddressID, BillToAddressID, ShipMethod, 
+                    CreditCardApprovalCode, SubTotal, TaxAmt, Freight, TotalDue, 
+                    Comment, Rowguid, ModifiedDate 
+                FROM SalesLT.SalesOrderHeader";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            salesOrderHeaders.Add(new SalesOrderHeader
+                            {
+                                SalesOrderID = reader.GetInt32(0),
+                                RevisionNumber = reader.GetByte(1),
+                                OrderDate = reader.GetDateTime(2),
+                                DueDate = reader.GetDateTime(3),
+                                ShipDate = reader.IsDBNull(4) ? (DateTime?)null : reader.GetDateTime(4),
+                                Status = reader.GetByte(5),
+                                OnlineOrderFlag = reader.GetBoolean(6),
+                                SalesOrderNumber = reader.GetString(7),
+                                PurchaseOrderNumber = reader.IsDBNull(8) ? null : reader.GetString(8),
+                                AccountNumber = reader.IsDBNull(9) ? null : reader.GetString(9),
+                                CustomerID = reader.GetInt32(10),
+                                ShipToAddressID = reader.IsDBNull(11) ? (int?)null : reader.GetInt32(11),
+                                BillToAddressID = reader.IsDBNull(12) ? (int?)null : reader.GetInt32(12),
+                                ShipMethod = reader.GetString(13),
+                                CreditCardApprovalCode = reader.IsDBNull(14) ? null : reader.GetString(14),
+                                SubTotal = reader.GetDecimal(15),
+                                TaxAmt = reader.GetDecimal(16),
+                                Freight = reader.GetDecimal(17),
+                                TotalDue = reader.GetDecimal(18), // Computed column
+                                Comment = reader.IsDBNull(19) ? null : reader.GetString(19),
+                                Rowguid = reader.GetGuid(20),
+                                ModifiedDate = reader.GetDateTime(21)
+                            });
+                        }
+                    }
+                }
+            }
+
+            return salesOrderHeaders;
+        }
+
     }
+
+
+
+
 }
---
+++ Linq/SalesOrderDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class SalesOrderDetail
+    {
+        public int SalesOrderID { get; set; } // Part of Primary Key
+        public int SalesOrderDetailID { get; set; } // Part of Primary Key (Identity)
+        public short OrderQty { get; set; } // smallint, Not Nullable
+        public int ProductID { get; set; } // Not Nullable
+        public decimal UnitPrice { get; set; } // money, Not Nullable
+        public decimal UnitPriceDiscount { get; set; } // money, Not Nullable
+        public decimal LineTotal { get; set; } // Computed column
+        public Guid Rowguid { get; set; } // uniqueidentifier, Not Nullable
+        public DateTime ModifiedDate { get; set; } // datetime, Not Nullable
+    }
+}
